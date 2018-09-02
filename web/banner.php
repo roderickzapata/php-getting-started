0c3899a7fbf1959d5930e35788c94cc6d79842f8
Roderick Zapata <40707147+roderickzapata@users.noreply.github.com>
2018-09-01 23:26:49 -0600

    Update banner.php

--- web/banner.php
+++ web/banner.php
@@ -1,1 +1,1 @@
-https://rylcotv.herokuapp.com/recargalo.png, https://play.google.com/store/apps/details?id=com.rylco.recargalo1
+https://rylcotv.herokuapp.com/recargalo.png, http://www.roderickzapata.com/
