9f838ed08c72680dc92616a78d54fc30956a3bd6
Roderick Zapata <40707147+roderickzapata@users.noreply.github.com>
2018-09-09 21:03:32 -0600

    Update banner.php

--- web/banner.php
+++ web/banner.php
@@ -1,1 +1,1 @@
-<a href="https://play.google.com/store/apps/details?id=com.rylco.recargalo1" target="_blank"><img src="https://rylcotv.herokuapp.com/recargalo.png" alt="Recargas Claro" width="350px" height="50px"></a>
+<script type="text/javascript" src="http://ad.leadbolt.net/show_app_ad.js?section_id=918175365"></script>
