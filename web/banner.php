e89bc358b7da8830faf9f2c7cb2b53fbc6a17180
Roderick Zapata <40707147+roderickzapata@users.noreply.github.com>
2018-09-10 22:50:25 -0600

    Update banner.php

--- web/banner.php
+++ web/banner.php
@@ -1,1 +1,1 @@
-<a href="http://www.youtube.com/subscription_center?add_user=roderickzapata" target="_blank"><img src="https://rylcotv.herokuapp.com/suscriptores.png" alt="Recargas Claro" width="350px" height="50px"></a>
+<a href="https://www.youtube.com/watch?v=kbZYJ5K7Uz4" target="_blank"><img src="https://rylcotv.herokuapp.com/suscriptores.png" alt="Recargas Claro" width="350px" height="50px"></a>
