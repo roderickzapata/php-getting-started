156ae6cb16e3b9a359ed065330e0f5fd346580af
Roderick Zapata <40707147+roderickzapata@users.noreply.github.com>
2018-10-14 17:50:32 -0600

    Create pushsuperzanyer.php

---
+++ web/pushsuperzanyer.php
@@ -0,0 +1,65 @@
+<?PHP
+function sendMessage() {
+    $content      = array(
+        "en" => 'Nuevo Pedido'
+    );
+    $hashes_array = array();
+    array_push($hashes_array, array(
+        "id" => "like-button",
+        "text" => "Like",
+        "icon" => "http://i.imgur.com/N8SN8ZS.png",
+        "url" => "https://zanyer.com"
+    ));
+    array_push($hashes_array, array(
+        "id" => "like-button-2",
+        "text" => "Like2",
+        "icon" => "http://i.imgur.com/N8SN8ZS.png",
+        "url" => "https://zanyer.com"
+    ));
+    $fields = array(
+        'app_id' => "f55e0ba5-3d08-4a0c-8a22-c7148ce4f66b",
+        'included_segments' => array(
+            'All'
+        ),
+        'data' => array(
+            "foo" => "bar"
+        ),
+        'contents' => $content,
+        'web_buttons' => $hashes_array
+    );
+    
+    $fields = json_encode($fields);
+    print("\nJSON sent:\n");
+    print($fields);
+    
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, "https://onesignal.com/api/v1/notifications");
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+        'Content-Type: application/json; charset=utf-8',
+        'Authorization: Basic YmRiMDg3YTItMGM3MS00YjBjLTlkZDAtZDUzMDgxNjM2NzQ2'
+    ));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+    curl_setopt($ch, CURLOPT_HEADER, FALSE);
+    curl_setopt($ch, CURLOPT_POST, TRUE);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+    
+    $response = curl_exec($ch);
+    curl_close($ch);
+    
+    return $response;
+}
+
+$response = sendMessage();
+$return["allresponses"] = $response;
+$return = json_encode($return);
+
+$data = json_decode($response, true);
+print_r($data);
+$id = $data['id'];
+print_r($id);
+
+print("\n\nJSON received:\n");
+print($return);
+print("\n");
+?>
