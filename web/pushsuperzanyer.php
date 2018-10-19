065b8d836024f35e98c2fe7d1408806c63389d00
Roderick Zapata <40707147+roderickzapata@users.noreply.github.com>
2018-10-18 22:14:05 -0600

    Update pushsuperzanyer.php

--- web/pushsuperzanyer.php
+++ web/pushsuperzanyer.php
@@ -1,62 +1,41 @@
 <?PHP
-function sendMessage() {
-    $content      = array(
-        "en" => 'Nuevo Pedido'
-    );
-    $hashes_array = array();
-    array_push($hashes_array, array(
-        "id" => "like-button",
-        "text" => "Like",
-        "icon" => "http://i.imgur.com/N8SN8ZS.png",
-        "url" => "https://zanyer.com"
-    ));
-    array_push($hashes_array, array(
-        "id" => "like-button-2",
-        "text" => "Like2",
-        "icon" => "http://i.imgur.com/N8SN8ZS.png",
-        "url" => "https://zanyer.com"
-    ));
-    $fields = array(
-        'app_id' => "f55e0ba5-3d08-4a0c-8a22-c7148ce4f66b",
-        'included_segments' => array(
-            'All'
-        ),
-        'data' => array(
-            "foo" => "bar"
-        ),
-        'contents' => $content,
-        'web_buttons' => $hashes_array
-    );
-    
-    $fields = json_encode($fields);
-    print("\nJSON sent:\n");
-    print($fields);
-    
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, "https://onesignal.com/api/v1/notifications");
-    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-        'Content-Type: application/json; charset=utf-8',
-        'Authorization: Basic YmRiMDg3YTItMGM3MS00YjBjLTlkZDAtZDUzMDgxNjM2NzQ2'
-    ));
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-    curl_setopt($ch, CURLOPT_HEADER, FALSE);
-    curl_setopt($ch, CURLOPT_POST, TRUE);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-    
-    $response = curl_exec($ch);
-    curl_close($ch);
-    
-    return $response;
+$custom = $_GET["custom"];
+if ($custom == null) {
+     $ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL, "https://onesignal.com/api/v1/notifications");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, "{\"app_id\": \"f55e0ba5-3d08-4a0c-8a22-c7148ce4f66b\",\n\"contents\": {\"en\": \"Nuevo Pedido\"},\n\"included_segments\": [\"Subscribed Users\"]}");
+curl_setopt($ch, CURLOPT_POST, 1);
+
+$headers = array();
+$headers[] = "Content-Type: application/json; charset=utf-8";
+$headers[] = "Authorization: Basic YmRiMDg3YTItMGM3MS00YjBjLTlkZDAtZDUzMDgxNjM2NzQ2";
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+$result = curl_exec($ch);
+if (curl_errno($ch)) {
+    echo 'Error:' . curl_error($ch);
 }
-$response = sendMessage();
-$return["allresponses"] = $response;
-$return = json_encode($return);
-$data = json_decode($response, true);
-print_r($data);
-$id = $data['id'];
-print_r($id);
-print("\n\nJSON received:\n");
-print($return);
-print("\n");
+curl_close ($ch);
+}
+else{
+    $ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL, "https://onesignal.com/api/v1/notifications");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, "{\"app_id\": \"f55e0ba5-3d08-4a0c-8a22-c7148ce4f66b\",\n\"contents\": {\"en\": \"$custom\"},\n\"included_segments\": [\"Subscribed Users\"]}");
+curl_setopt($ch, CURLOPT_POST, 1);
+
+$headers = array();
+$headers[] = "Content-Type: application/json; charset=utf-8";
+$headers[] = "Authorization: Basic YmRiMDg3YTItMGM3MS00YjBjLTlkZDAtZDUzMDgxNjM2NzQ2";
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+$result = curl_exec($ch);
+if (curl_errno($ch)) {
+    echo 'Error:' . curl_error($ch);
+}
+curl_close ($ch);
+}
 ?>
